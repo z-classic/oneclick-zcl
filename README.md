24ea07c0b8d4e6c743f73fb814629854785349d9
Jon Layton <jon@seequ.com>
2018-01-30 02:23:53 -0600

    Added optiminer fetching

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # oneclick-zcl
 Zclassic One-Click Miner (Equihash)
 
-```
-sudo apt-get install python-qt4
 
+
+```
+sudo apt-get install wget python-qt4
 pip install pyside
+
+wget http://download.optiminer.pl/optiminer-equihash-2.1.2.tar.gz
+tar xzf optiminer-equihash-2.1.2
+
 python2.7 tray.py
 ```
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# TODO Start from a config file
+# TODO Run from python, with contents of a config file
 
 # Running optiminer-equihash-2.1.2: https://github.com/Optiminer/OptiminerEquihash
-./optiminer-equihash -s $POOL -u $USER -p $PASSWORD --watchdog-timeout 30 -a equihash200_9 --watchdog-cmd "./watchdog-cmd.sh" $ADDITIONAL $@
+#./optiminer-equihash-2.1.2/optiminer-equihash -s $POOL -u $USER -p $PASSWORD --watchdog-timeout 30 -a equihash200_9 --watchdog-cmd "./watchdog-cmd.sh" $ADDITIONAL $@
